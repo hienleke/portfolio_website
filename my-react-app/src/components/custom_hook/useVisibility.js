a57b364c893fee2da7a3a37036939e6074586ec4
le ke hien <killer0121906@gmail.com>
2024-12-31 14:47:09 +0700

    -- fix nav

--- my-react-app/src/components/custom_hook/useVisibility.js
+++ my-react-app/src/components/custom_hook/useVisibility.js
@@ -1,38 +1,39 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import  setCurrentComponent  from '../../redux/action'; // Import the action
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import setCurrentComponent from "../../redux/action"; // Import the action
 
 const useVisibility = (elementRef, componentName) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
+	const isComponentInView = () => {
+		if (elementRef.current) {
+			const rect = elementRef.current.getBoundingClientRect();
+			if (0 < window.scrollY && window.scrollY < 10) {
+				return true;
+			}
 
-  const isComponentInView = () => {
-    if (elementRef.current) {
-      const rect = elementRef.current.getBoundingClientRect();
-      if (0 < window.scrollY && window.scrollY < 10 && componentName === 'About')
-        return true
-      return rect.top < window.innerHeight && rect.bottom >= 0;
-    }
-    return false;
-  };
+			return rect.top < window.innerHeight && rect.bottom >= 0;
+		}
+		return false;
+	};
 
-  
-  const handleVisibilityChange = () => {
+	const handleVisibilityChange = () => {
     if (isComponentInView()) {
-      dispatch(setCurrentComponent(componentName));  
-    }
-  };
+      if (0 < window.scrollY && window.scrollY < 10)  {
+        dispatch(setCurrentComponent("About"));
+      }
+      else
+			dispatch(setCurrentComponent(componentName));
+		}
+	};
 
-  useEffect(() => {
+	useEffect(() => {
+		window.addEventListener("scroll", handleVisibilityChange);
 
-    window.addEventListener('scroll', handleVisibilityChange);
-
-
-    return () => {
-      window.removeEventListener('scroll', handleVisibilityChange);
-    };
-  }, [elementRef, dispatch]);
-
+		return () => {
+			window.removeEventListener("scroll", handleVisibilityChange);
+		};
+	}, [elementRef, dispatch]);
 };
 
 export default useVisibility;
